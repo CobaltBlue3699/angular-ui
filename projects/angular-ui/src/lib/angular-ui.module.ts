b967a7e303cea38676505d123e86287ccb1ce0a0
johnson.lee <johnson.lee3699@gmail.com>
2023-03-24 12:27:43 +0800

    delete table module

--- projects/angular-ui/src/lib/angular-ui.module.ts
+++ projects/angular-ui/src/lib/angular-ui.module.ts
@@ -1,12 +1,11 @@
 import { TooltipModule, TooltipDirective } from './tooltip';
 import { NgModule } from '@angular/core';
 import { AlertModule, AlertService } from './alert';
-import { TableModule } from './table';
 
 @NgModule({
   declarations: [],
-  imports: [AlertModule, TableModule, TooltipModule],
-  exports: [AlertModule, TableModule, TooltipModule],
+  imports: [AlertModule, TooltipModule],
+  exports: [AlertModule, TooltipModule],
   providers: [AlertService, TooltipDirective],
 })
 export class AngularUIModule {}
